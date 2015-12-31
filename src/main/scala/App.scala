e1a8c6c0d924fe7fee772ff0add249dd8891aa8a
yuya <life.of.151a@gmail.com>
2015-12-31 13:26:28 +0900

    first commit

---
+++ src/main/scala/App.scala
@@ -0,0 +1,11 @@
+/**
+  * App.java
+  *
+  * @since 2015/12/31
+  *
+  *        Copyright 2015 fuji-151a
+  *        All Rights Reserved.
+  */
+object App {
+
+}
---
+++ src/test/scala/App$Test.scala
@@ -0,0 +1,11 @@
+/**
+  * App$Test.java
+  *
+  * @since 2015/12/31
+  *
+  *        Copyright 2015 fuji-151a
+  *        All Rights Reserved.
+  */
+class App$Test extends org.scalatest.FunSuite {
+
+}
